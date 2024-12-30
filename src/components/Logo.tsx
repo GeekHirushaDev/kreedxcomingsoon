48c86b325ed1d211f4361f0a51f941476905665f
Hirusha Dayarathna <145102279+GeekHirushaDev@users.noreply.github.com>
2024-12-31 02:31:10 +0530

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ interface LogoProps {
 export function Logo({ className = '' }: LogoProps) {
   return (
     <div className={`font-bold text-3xl select-none ${className}`}>
-      <img className="text-white" src="src/components/kreedx_light.png" width={200} height={200} alt="KreedX Logo" />
+      <img className="text-white" src="https://kreedx.com/kreedx_light.png" width={200} height={200} alt="KreedX Logo" />
     </div>
   );
 }
